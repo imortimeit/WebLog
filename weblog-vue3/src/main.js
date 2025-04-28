b3a133f0a064732dbe42876d70ce7e0f30376982
imortimeit <2102321426@qq.com>
2025-04-28 10:47:53 +0800

    登录模块-登录页面设计：响应式页面

--- weblog-vue3/src/main.js
+++ weblog-vue3/src/main.js
@@ -4,9 +4,15 @@ import { createApp } from 'vue'   // 引入 createApp 方法
 import App from '@/App.vue'     // 引入 App.vue 组件
 // 导入路由
 import router from '@/router'
+// 导入 Element Plus 图标
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 const app = createApp(App)   // 创建应用，并将 App 根组件挂载到 <div id="#app"></div> 中
 
+// 引入图标（例如登录页面的图标）
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
 // 应用路由
 app.use(router)
 app.mount('#app')
