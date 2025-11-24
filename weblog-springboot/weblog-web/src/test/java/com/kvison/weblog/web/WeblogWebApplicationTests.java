239b79159d5992f86bb49fe94e44353a895fec59
imortimeit <2102321426@qq.com>
2025-11-24 08:54:23 +0800

    单元测试增加使用mp增加用户

--- weblog-springboot/weblog-web/src/test/java/com/kvison/weblog/web/WeblogWebApplicationTests.java
+++ weblog-springboot/weblog-web/src/test/java/com/kvison/weblog/web/WeblogWebApplicationTests.java
@@ -1,16 +1,25 @@
 package com.kvison.weblog.web;
 
+import com.kvison.weblog.common.domain.dos.UserDO;
+import com.kvison.weblog.common.domain.mapper.UserMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Date;
 
 @Slf4j
 @SpringBootTest
 class WeblogWebApplicationTests {
 
+    @Autowired
+    private UserMapper userMapper;
+
     @Test
     void contextLoads() {
     }
+
     @Test
     void testlog() {
         log.info("info 日志");
@@ -19,6 +28,18 @@ class WeblogWebApplicationTests {
 
         String str = "kvison";
         log.info("str = {}", str);
+    }
+
+    @Test
+    void testUserMapper() {
+        UserDO kvison = UserDO.builder()
+                .username("kvison")
+                .password("123456")
+                .createTime(new Date())
+                .updateTime(new Date())
+                .isDeleted(false)
+                .build();
+        userMapper.insert(kvison);
     }
 
 }
